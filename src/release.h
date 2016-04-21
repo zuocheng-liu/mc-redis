42f61ded7f69a6f74d512aa017d7194a06b5409a
Zuocheng Liu <zuocheng.liu@gmail.com>
2016-04-21 12:04:55 +0800

    remove .o files

--- src/release.h
+++ src/release.h
@@ -1,3 +1,3 @@
-#define REDIS_GIT_SHA1 "32535ff5"
-#define REDIS_GIT_DIRTY "147"
-#define REDIS_BUILD_ID "zuocheng.net-1461155035"
+#define REDIS_GIT_SHA1 "b0041968"
+#define REDIS_GIT_DIRTY "0"
+#define REDIS_BUILD_ID "zuocheng.net-1461211474"
